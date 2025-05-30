8fbfa2d77b6dce9698aed82409c2c34f96871283
parryk10 <parryk010@gmail.com>
2025-05-30 12:11:00 +0200

    Update uzycie.js

--- Script/commands/uzycie.js
+++ Script/commands/uzycie.js
@@ -13,17 +13,13 @@ module.exports.run = async function({ api, event }) {
     const prefix = "/";
     const msg = 
 `${prefix}aizdj — użycie: ${prefix}aizdj [tekst do wygenerowania]
-${prefix}disco — użycie: ${prefix}disco
-${prefix}dodaj — użycie: ${prefix}dodaj [parametry]
+${prefix}disco — użycie: ${prefix}disco [max 25]
 ${prefix}giveaway — użycie: ${prefix}giveaway [create/details/join/roll/end] [IDGiveAway]
-${prefix}help — użycie: ${prefix}help
-${prefix}lista — użycie: ${prefix}lista
-${prefix}pobierz — użycie: ${prefix}pobierz [link lub coś do pobrania]
-${prefix}powiedz — użycie: ${prefix}powiedz [tekst]
+${prefix}pobierz — użycie: ${prefix}pobierz [odpowiedz na wiadomosc np glosowka/film/zdjecie]
+${prefix}powiedz — użycie: ${prefix}powiedz [jezyk] [tekst]
 ${prefix}uid — użycie: ${prefix}uid [tag użytkownika lub ID]
-${prefix}wlasciciel — użycie: ${prefix}wlasciciel
-${prefix}wybierz — użycie: ${prefix}wybierz [opcje oddzielone przecinkami]
-${prefix}zdj — użycie: ${prefix}zdj [tekst lub coś do zdjęcia]`;
+${prefix}wybierz — użycie: ${prefix}wybierz [opcje oddzielone myslnikiem]
+${prefix}zdj — użycie: ${prefix}zdj [co chcesz wyszukac-ile zdjec]`;
 
     return api.sendMessage(msg, event.threadID, event.messageID);
 };
