b1544d4e93ff0d0c69275b488c12aeb2390632e1
parryk10 <parryk010@gmail.com>
2025-05-30 12:29:00 +0200

    Update uzycie.js

--- Script/commands/uzycie.js
+++ Script/commands/uzycie.js
@@ -12,12 +12,15 @@ module.exports.config = {
 module.exports.run = async function({ api, event }) {
     const prefix = "/";
     const msg = 
-`${prefix}aizdj — użycie: ${prefix}aizdj [tekst do wygenerowania]
+`${prefix}aizdj — użycie: ${prefix}aizdj [tekst do wygenerowania (tylko po angielsku)]
 ${prefix}disco — użycie: ${prefix}disco [max 25]
+${prefix}drgs — użycie: ${prefix}drgs [nazwa drgs o ktorym chcesz sie doedukowac]
+${prefix}flux — użycie: ${prefix}flux [opis zdjecia ktore chcesz wygenerować]
 ${prefix}giveaway — użycie: ${prefix}giveaway [create/details/join/roll/end] [IDGiveAway]
 ${prefix}pobierz — użycie: ${prefix}pobierz [odpowiedz na wiadomosc np glosowka/film/zdjecie]
 ${prefix}powiedz — użycie: ${prefix}powiedz [jezyk] [tekst]
-${prefix}uid — użycie: ${prefix}uid [tag użytkownika lub ID]
+${prefix}sing — użycie: ${prefix}sing [tytul utworu ktory chcesz aby zostal wyslany]
+${prefix}ttt — użycie: ${prefix}ttt [liczby od 1 do 9]
 ${prefix}wybierz — użycie: ${prefix}wybierz [opcje oddzielone myslnikiem]
 ${prefix}zdj — użycie: ${prefix}zdj [co chcesz wyszukac-ile zdjec]`;
 
