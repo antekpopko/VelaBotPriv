afd97e3710c39f6e8daab0c62117af31473a376a
parryk10 <parryk010@gmail.com>
2025-05-31 10:43:34 +0200

    Update pfp.js

--- Script/commands/pfp.js
+++ Script/commands/pfp.js
@@ -3,10 +3,10 @@ const axios = require("axios");
 module.exports = {
   config: {
     name: "pfp",
-    version: "1.1",
+    version: "1.0",
     hasPermission: 0,
     usePrefix: true,
-    credits: "Dipto (tłumaczenie i ulepszenia: January)",
+    credits: "Dipto (tłumaczenie i uproszczenie: January)",
     description: "Pobierz informacje o użytkowniku i jego zdjęcie profilowe",
     commandCategory: "informacje",
     cooldowns: 5,
@@ -20,47 +20,34 @@ module.exports = {
 
     const userInfo = await api.getUserInfo(uid);
     const user = userInfo[uid];
-
     const avatarUrl = `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
-    const profileUrl = `https://facebook.com/${uid}`;
 
     // Tłumaczenie płci
     let genderText = "❓ Nieznana";
     if (user.gender === 1) genderText = "👩 Kobieta";
     else if (user.gender === 2) genderText = "👨 Mężczyzna";
 
-    // Dodatkowe informacje
-    const name = user.name || "Brak";
-    const nickname = user.alternateName || "Brak";
-    const vanity = user.vanity || "Brak";
-    const birthday = user.isBirthday !== false ? user.isBirthday : "Ukryte";
-    const isFriend = user.isFriend ? "✅ Tak" : "❌ Nie";
-    const isVerified = user.isVerified ? "✅ Tak" : "❌ Nie";
-    const mutualFriends = user.mutualFriends !== undefined ? `${user.mutualFriends}` : "Nieznana";
-
+    // Dane użytkownika
     const info = `
 📄 Informacje o użytkowniku:
 
-👤 Imię i nazwisko: ${name}
-🔖 Pseudonim: ${nickname}
+👤 Imię i nazwisko: ${user.name || "Brak"}
+🔖 Pseudonim: ${user.alternateName || "Brak"}
 🆔 UID: ${uid}
-📛 Nazwa użytkownika: ${vanity}
+📛 Nazwa użytkownika: ${user.vanity || "Brak"}
 ⚧️ Płeć: ${genderText}
-🎂 Urodziny: ${birthday}
-👥 Znajomy bota: ${isFriend}
-🔵 Zweryfikowane konto: ${isVerified}
-🤝 Wspólnych znajomych z botem: ${mutualFriends}
-🔗 Link do profilu: ${profileUrl}
+🎂 Urodziny: ${user.isBirthday !== false ? user.isBirthday : "Ukryte"}
+👥 Znajomy bota: ${user.isFriend ? "✅ Tak" : "❌ Nie"}
+🌐 Link do profilu: https://facebook.com/${uid}
     `.trim();
 
-    // Załaduj zdjęcie profilowe
+    // Wczytaj zdjęcie profilowe
     const avatarStream = (await axios.get(avatarUrl, { responseType: "stream" })).data;
 
     // Wyślij wiadomość
     return api.sendMessage({
       body: info,
       attachment: avatarStream,
-      url: profileUrl // opcjonalne, ale niektóre boty obsługują klikane linki przez to pole
     }, event.threadID, event.messageID);
   },
 };
