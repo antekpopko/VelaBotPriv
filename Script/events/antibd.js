e5145641fb8622c6594048695f6e64ffb7ce035e
parryk10 <parryk010@gmail.com>
2025-06-02 18:20:33 +0200

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -13,13 +13,22 @@ module.exports.run = async function ({ api, event, Users }) {
 
   if (logMessageData.participant_id == botID && author != botID) {
     const defaultNickname = `[ ${PREFIX} ] • ${BOTNAME || "BOT"}`;
-    
+
     // Przywróć oryginalny nick
-    api.changeNickname(defaultNickname, threadID, botID);
+    try {
+      await api.changeNickname(defaultNickname, threadID, botID);
+    } catch (err) {
+      console.error("Nie udało się zmienić nicku:", err);
+    }
 
     // Pobierz nazwę użytkownika
-    const info = await Users.getData(author);
-    const name = info?.name || "Ktoś";
+    let name = "Ktoś";
+    try {
+      const info = await Users.getData(author);
+      if (info?.name && info.name !== "Facebook User") name = info.name;
+    } catch (e) {
+      console.warn("Nie udało się pobrać danych użytkownika:", e);
+    }
 
     return api.sendMessage(
       `${name}, nie próbuj zmieniać mojego nicku! 😼\nTylko ja decyduję, jak się nazywam.`,
